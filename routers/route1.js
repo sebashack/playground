a12d2be6e11252774b089a7c9ba1a8eee7d3cc9b
Sebas <spulido@stackbuildersl.com>
2016-12-21 13:15:30 -0500

    deleting js files

--- routers/route1.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var express = require("express");
-var handlers_1 = require("./handlers");
-exports.router1 = express.Router();
-//Router is decoupled from handler.
-exports.router1.route('/endpoint1')
-    .get(handlers_1.endpoint1Handler);
--- routers/route2.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var express = require("express");
-var handlers_1 = require("./handlers");
-exports.router2 = express.Router();
-exports.router2.get('/twilio', handlers_1.twilioHandler);
